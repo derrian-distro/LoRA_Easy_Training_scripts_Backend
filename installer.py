6623ce9252ec3e3bc4f40a60977780ab0d05cc6e
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2024-03-15 16:27:03 -0400

    update torch to 2.2.1

--- installer.py
+++ installer.py
@@ -85,7 +85,7 @@ def setup_accelerate(platform: str) -> None:
 
 def setup_venv(venv_pip):
     subprocess.check_call(
-        f"{venv_pip} install -U torch==2.2.0 torchvision==0.17.0 --index-url https://download.pytorch.org/whl/cu121",
+        f"{venv_pip} install -U torch==2.2.1 torchvision==0.17.1 --index-url https://download.pytorch.org/whl/cu121",
         shell=PLATFORM == "linux",
     )
     subprocess.check_call(
