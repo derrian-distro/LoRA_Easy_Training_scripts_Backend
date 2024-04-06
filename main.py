a05ec1e6835493f4642a852f82a22de103bd4c2d
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2024-04-06 01:20:22 -0400

    changed all of the responses to a JSONResponse object, added support for lora resize

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from starlette.applications import Starlette
 from starlette.requests import Request
-from starlette.responses import Response
+from starlette.responses import JSONResponse
 from starlette.routing import Route
 from starlette import status
 import json
@@ -21,18 +21,18 @@ if not Path("runtime_store").exists():
     Path("runtime_store").mkdir()
 
 
-async def stop_server(_: Request) -> Response:
+async def stop_server(_: Request) -> JSONResponse:
     global server
     if app.state.TRAINING_THREAD and app.state.TRAINING_THREAD.poll() is None:
-        return Response(json.dumps({"detail": "training still running"}))
+        return JSONResponse({"detail": "training still running"})
     server.should_exit = True
     server.force_exit = True
 
 
-async def start_tunnel_service(request: Request) -> Response:
+async def start_tunnel_service(request: Request) -> JSONResponse:
     config_data = json.loads(app.state.CONFIG.read_text())
     if app.state.TUNNEL:
-        return Response(json.dumps({"service_started": False}), status_code=409)
+        return JSONResponse({"service_started": False}, status_code=409)
     app.state.TUNNEL = create_tunnel(config_data)
     if isinstance(app.state.TUNNEL, CloudflaredTunnel):
         config_path = request.query_params.get(
@@ -43,23 +43,23 @@ async def start_tunnel_service(request: Request) -> Response:
         app.state.TUNNEL.run_tunnel(config=Path(config_path) if config_path else None)
     else:
         app.state.TUNNEL.run_tunnel()
-    return Response(json.dumps({"service_started": bool(app.state.TUNNEL)}))
+    return JSONResponse({"service_started": bool(app.state.TUNNEL)})
 
 
-async def kill_tunnel_service(_: Request) -> Response:
+async def kill_tunnel_service(_: Request) -> JSONResponse:
     if not app.state.TUNNEL:
-        return Response(
-            json.dumps({"killed": False, "reason": "No Tunnel Service Running"}),
+        return JSONResponse(
+            {"killed": False, "reason": "No Tunnel Service Running"},
             status_code=400,
         )
     app.state.TUNNEL.kill_service()
     app.state.TUNNEL = None
-    return Response(
-        json.dumps({"killed": True, "reason": "Tunnel Service Successfully Killed"})
+    return JSONResponse(
+        {"killed": True, "reason": "Tunnel Service Successfully Killed"}
     )
 
 
-async def check_path(request: Request) -> Response:
+async def check_path(request: Request) -> JSONResponse:
     body = await request.body()
     body = json.loads(body)
     file_path = Path(body["path"])
@@ -72,13 +72,13 @@ async def check_path(request: Request) -> Response:
         and file_path.suffix in body["extensions"]
     ):
         valid = True
-    return Response(json.dumps({"valid": valid}))
+    return JSONResponse({"valid": valid})
 
 
-async def validate_inputs(request: Request) -> Response:
+async def validate_inputs(request: Request) -> JSONResponse:
     if app.state.TRAINING_THREAD and app.state.TRAINING_THREAD.poll() is None:
-        return Response(
-            json.dumps({"detail": "training already running"}),
+        return JSONResponse(
+            {"detail": "Training Already Running"},
             status_code=status.HTTP_409_CONFLICT,
         )
     body = await request.body()
@@ -89,31 +89,30 @@ async def validate_inputs(request: Request) -> Response:
         output_args, _ = process_args(args)
         output_dataset_args, _ = process_dataset_args(dataset_args)
         final_args = {"args": output_args, "dataset": output_dataset_args, "tags": tags}
-        return Response(json.dumps(final_args, indent=2))
-    return Response(
-        json.dumps(errors),
+        return JSONResponse(final_args)
+    return JSONResponse(
+        errors,
         status_code=status.HTTP_400_BAD_REQUEST,
     )
 
 
-async def is_training(_: Request) -> Response:
+async def is_training(_: Request) -> JSONResponse:
     exit_id = app.state.TRAINING_THREAD.poll() if app.state.TRAINING_THREAD else 0
-    return Response(
-        json.dumps(
-            {
-                "training": exit_id is None,
-                "errored": exit_id is not None and exit_id != 0,
-            }
-        )
+    return JSONResponse(
+        {
+            "training": exit_id is None,
+            "errored": exit_id is not None and exit_id != 0,
+        }
     )
 
 
-async def start_training(request: Request) -> Response:
+async def start_training(request: Request) -> JSONResponse:
     if app.state.TRAINING_THREAD and app.state.TRAINING_THREAD.poll() is None:
-        return Response(
-            json.dumps({"detail": "Training Already Running"}),
+        return JSONResponse(
+            {"detail": "Training Already Running"},
             status_code=status.HTTP_409_CONFLICT,
         )
+
     is_sdxl = request.query_params.get("sdxl", False)
     if is_sdxl:
         app.state.SD_TYPE = "sdxl_train_network.py"
@@ -124,11 +123,10 @@ async def start_training(request: Request) -> Response:
     config = Path("runtime_store/config.toml")
     dataset = Path("runtime_store/dataset.toml")
     if not config.is_file() or not dataset.is_file():
-        return Response(
-            json.dumps({"detail": "No Previously Validated Args"}),
+        return JSONResponse(
+            {"detail": "No Previously Validated Args"},
             status_code=status.HTTP_400_BAD_REQUEST,
         )
-    print(app.state.SD_TYPE)
     app.state.TRAINING_THREAD = subprocess.Popen(
         [
             f"{python}",
@@ -149,23 +147,38 @@ async def start_training(request: Request) -> Response:
     ):
         app.state.MONITOR_THREAD = Thread(target=monitor_training_thread, daemon=True)
         app.state.MONITOR_THREAD.start()
-    return Response(json.dumps({"detail": "Training Started", "training": True}))
+    return JSONResponse({"detail": "Training Started", "training": True})
 
 
-async def stop_training(request: Request) -> Response:
+async def stop_training(request: Request) -> JSONResponse:
     force = bool(request.query_params.get("force", False))
     if not app.state.TRAINING_THREAD and app.state.TRAINING_THREAD.poll() is not None:
-        return Response(
-            json.dumps({"detail": "Not Currently Training"}),
+        return JSONResponse(
+            {"detail": "Not Currently Training"},
             status_code=status.HTTP_400_BAD_REQUEST,
         )
     if force:
         app.state.TRAINING_THREAD.stderr = None
         app.state.TRAINING_THREAD.kill()
-        return Response(json.dumps({"detail": "Training Thread Killed"}))
+        return JSONResponse({"detail": "Training Thread Killed"})
     else:
         app.state.TRAINING_THREAD.terminate()
-    return Response(json.dumps({"detail": "Training Thread Requested to Die"}))
+    return JSONResponse({"detail": "Training Thread Requested to Die"})
+
+
+async def start_resize(request: Request) -> JSONResponse:
+    if app.state.TRAINING_THREAD and app.state.TRAINING_THREAD.poll() is None:
+        return JSONResponse(
+            {"detail": "Training Already Running"}, status_code=status.HTTP_409_CONFLICT
+        )
+    data = await request.body()
+    data: list[str] = json.loads(data)
+    print(data)
+    python = sys.executable
+    app.state.TRAINING_THREAD = subprocess.Popen(
+        [python, f"{Path('utils/resize_lora.py').resolve()}"] + data
+    )
+    return JSONResponse({"detail": "Resizing Started"})
 
 
 def monitor_training_thread():
@@ -186,6 +199,7 @@ routes = [
     Route("/is_training", is_training, methods=["GET"]),
     Route("/train", start_training, methods=["GET"]),
     Route("/stop_training", stop_training, methods=["GET"]),
+    Route("/resize", start_resize, methods=["POST"]),
 ]
 
 app = Starlette(debug=True, routes=routes)
