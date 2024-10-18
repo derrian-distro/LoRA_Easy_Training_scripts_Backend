124838c43c3f12ab5fad2faf54e85569501431d0
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2024-10-18 17:03:16 -0400

    update sd-scripts, add install for xformers for torch 2.4.1

--- installer.py
+++ installer.py
@@ -80,15 +80,15 @@ def setup_accelerate(platform: str) -> None:
 
 def setup_venv(venv_pip):
     subprocess.check_call(
-        f"{venv_pip} install -U torch torchvision --index-url https://download.pytorch.org/whl/cu124",
+        f"{venv_pip} install -U torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cu124",
         shell=PLATFORM == "linux",
     )
     if PLATFORM == "windows":
         subprocess.check_call("venv\\Scripts\\python.exe ..\\fix_torch.py")
-    # subprocess.check_call(
-    #     f"{venv_pip} install -U xformers==0.0.28.dev881 --index-url https://download.pytorch.org/whl/cu124",
-    #     shell=PLATFORM == "linux",
-    # )
+    subprocess.check_call(
+        f"{venv_pip} install -U xformers==0.0.28.post1 --index-url https://download.pytorch.org/whl/cu124",
+        shell=PLATFORM == "linux",
+    )
     subprocess.check_call(f"{venv_pip} install -U -r requirements.txt", shell=PLATFORM == "linux")
     subprocess.check_call(f"{venv_pip} install -U ../custom_scheduler/.", shell=PLATFORM == "linux")
     subprocess.check_call(f"{venv_pip} install -U -r ../requirements.txt", shell=PLATFORM == "linux")
